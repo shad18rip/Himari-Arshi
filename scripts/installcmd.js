14a517257adb3fd77628290dbfd52a03aae812e4
shad18rip <sabinarozy1977@gmail.com>
2026-03-14 16:10:12 +0600

    Create installcmd.js

---
+++ scripts/installcmd.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+module.exports = {
+  config: {
+    name: "installcmd",
+    version: "1.0",
+    author: "Shad",
+    role: 2,
+    shortDescription: "Install command from chat"
+  },
+
+  onStart: async function ({ message, event }) {
+
+    if (!event.messageReply)
+      return message.reply("Reply to command code");
+
+    const code = event.messageReply.body;
+
+    const match = code.match(/name:\s*["'](.*?)["']/);
+
+    if (!match)
+      return message.reply("Command name not found");
+
+    const name = match[1];
+
+    fs.writeFileSync(`scripts/cmds/${name}.js`, code);
+
+    message.reply(`✅ Command "${name}" installed`);
+  }
+};
